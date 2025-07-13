ddadb179cc0b9696e3f2a12b25d589a6fad68d6c
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-13 15:00:31 -0500

    ejercicio extra de pila, simulador de navegación web

--- Pilas y colas.py
+++ Pilas y colas.py
@@ -4,15 +4,74 @@ Ejercicio
 
 #Pila/Stack (LIFO)
 
-stack = []
+stack = []  #Creación de lista para simular una pila
 
-#Push
-stack.append("1")
-stack.append("2")
-stack.append("3")
+#Push: añadir elementos a la pila
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
 print(stack)
 
-#Pop
-stack_item = stack[len(stack)-1]
+#Pop: eliminar el último elemento añadido
+stack_item = stack[len(stack)-1] 
+del stack[len(stack)-1] #Eliminamos un elemento dependiendo (-n) de la longitud de la pila
 print(stack_item)
 print(stack)
+
+print(stack.pop()) #Eliminamos el último elemento añadido
+print(stack)
+
+print("--------------------")
+
+#Cola /Queue (FIFO)
+
+queue = []  #Creación de lista para simular una cola
+
+#Añadir elementos a la cola
+queue.append("a")
+queue.append("b")
+queue.append("c")
+queue.append("d")
+print(queue)
+
+#Eliminar el primer elemento añadido
+queue_item = queue[0] 
+del queue[0] 
+print(queue_item)
+print(queue)
+
+print(queue.pop(0))  #Eliminamos el primer elemento añadido
+print(queue)
+
+"""
+Extra
+"""
+
+#Web
+
+def navigation_web():
+
+    stack = []
+    
+    while True:
+        action = input("introduce una url o inserta los comandos 'atras' , 'adelante' o 'salir': ")
+
+        if action == "salir":
+            print("Has salido de la navegación web.")
+            break
+        elif action == "atras":
+            if len(stack) > 0:
+                stack.pop()
+        elif action == "adelante":
+            print("No puedes avanzar, no hay páginas adelante.")
+            continue
+        else:
+            stack.append(action)
+        
+        if len(stack) > 0:
+            print(f"Estás en la página web: https://{stack[len(stack)-1]}")
+        else:
+            print("Inicio.")
+
+navigation_web() 
